ec9929e6828c1fd6e43e343502138a3ce625d55d
Hossein Moradi Davijani <moradidavijani@gmail.com>
2017-10-09 21:13:26 +0330

    protect get users route

--- api/users/users.routes.js
+++ api/users/users.routes.js
@@ -1,6 +1,13 @@
 module.exports = (app) => {
-	let controller = require('./users.controller'),
-	    generalRoutes = require('../general/general.routes')
+	let controller     = require('./users.controller'),
+	    authMiddleware = require('../auth/auth.middleware'),
+	    generalRoutes  = require('../general/general.routes')
 	
+	app.route('/users')
+		.get(authMiddleware)
+	
+	app.route('/users/:id')
+		.get(authMiddleware)
+		
 	generalRoutes(app, 'users', controller)
 }
