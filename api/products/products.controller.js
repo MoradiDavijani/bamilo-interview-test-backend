f787fdb6ce7fbbf3674b72e51a1365c5a2a459fa
Hossein Moradi Davijani <moradidavijani@gmail.com>
2017-10-12 07:34:36 +0330

    add search and reserve route for products

--- api/products/products.controller.js
+++ api/products/products.controller.js
@@ -17,3 +17,56 @@ exports.getAll = (req, callback) => {
 		.populate('category')
 		.populate('attributes.attribute')
 }
+
+exports.search = (req, callback) => {
+	let categories = (req.query.categories && req.query.categories.split(',')) || []
+	
+	let query = {}
+	if (categories.length) {
+		query = {
+			category: { $in: categories }
+		}
+	}
+	
+	let promises = [
+		Products
+			.find(query)
+			.skip(+req.query.skip)
+			.limit(+req.query.limit)
+			.exec(),
+		Products
+			.find(query)
+			.count()
+			.exec()
+	]
+	Promise.all(promises)
+		.then(([products, total]) => {
+			callback({
+				meta: { total },
+				data: { products }
+			})
+		})
+		.catch((err) => {
+			callback(err)
+		})
+}
+
+exports.reserve = (req, callback) => {
+	Products.findOneAndUpdate({ _id: req.params.id, quantity: { $gte: 1 } }, { $inc: { quantity: -1 } },
+		(err, result) => {
+			if (err) {
+				callback(err, result)
+				return
+			}
+			if (!result) {
+				callback({
+					status: 404,
+					message: 'Unfortunately this product is not available at the time'
+				})
+				return
+			}
+			callback(null, {
+				message: 'The product successfully reserved'
+			})
+		})
+}
--- api/products/products.routes.js
+++ api/products/products.routes.js
@@ -1,7 +1,10 @@
 let controller            = require('./products.controller'),
     router                = require('express').Router(),
+    { requestDecorator }  = require('../helpers/request.decorator'),
     registerGeneralRoutes = require('../general/general.routes')
 
 registerGeneralRoutes(router, controller)
+router
+	.post('/reserve/:id', requestDecorator(controller.reserve))
 
 module.exports = router
